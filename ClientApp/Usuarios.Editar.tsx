299b92acd82f92956efaca7c1d4242261fa7514a
ANGELDANIELCX <angeldaniel.ramirezhernadez.gen23@cbtis105.edu.mx>
2025-06-19 09:30:53 -0600

    fin

--- ClientApp/Usuarios.Editar.tsx
+++ ClientApp/Usuarios.Editar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 interface UsuariosEditarProps{
@@ -11,7 +11,27 @@ const UsuariosEditar = ({id} : UsuariosEditarProps) =>{
     const [correo, setCorreo] = useState("");
     const [password, setPassword] = useState("");
 
-    const guardar = () => {
+    const obtenerUsuarioExistente = async() => {
+        const resp = await fetch("/api/usuarios/"+ idUsuario);
+        if(resp.ok){
+            const datos = await resp.json();
+            setNombre(datos.Nombre);
+            setCorreo(datos.Correo);
+            setPassword(datos.Password);
+        }
+        else {
+            alert("Ocurrií un error al obtener el registro");
+        }
+    }
+    
+    useEffect(()=>{
+        if(idUsuario){
+            obtenerUsuarioExistente();  
+        }   
+    }, []);
+
+
+    const guardar = async  () => {
         const model = {
             "Nombre": nombre,
             "Correo": correo,
@@ -19,31 +39,60 @@ const UsuariosEditar = ({id} : UsuariosEditarProps) =>{
         }
 
         console.log (model);
+        let url = "/api/usuarios";  
+        let metodo = "POST";
+
+        if(idUsuario){
+            url += "/" + idUsuario;
+            metodo = "PUT";
+        }
+        const resp = await fetch(url, {
+            method: metodo,
+            body: JSON.stringify(model),
+            headers:{
+                 "Content-Type": "application/json"
+            }
+        });
+
+        if (resp.ok){
+            alert("El registro se guardo correctamente ");
+          
+                location.href = "/usuarios";
+            }
+        
+        else{
+            const error = await resp.text();
+            alert (error);
+        }
+
     }
 
         return (
         <>  
         <div className="container">
-            <h1>Editar Usuario</h1>
+            <h1>Editar Usuario{idUsuario}</h1>
         </div>
         <div className="container">
             <div className="row">
                 <div className="col-12">
                     <div className="mb-3">
                         <label>Nombre</label>
-                        <input type ="text" className="form-control" onChange={(e) => setNombre(e.target.value)}/> 
+                        <input type ="text" className="form-control" onChange={(e) => setNombre(e.target.value)} 
+                        value={nombre}/>
                     </div>
                 </div>
                                 <div className="col-12">
                     <div className="mb-3">
                         <label>Correo Electronico</label>
-                        <input type ="email" className="form-control" onChange={(e) => setCorreo(e.target.value)}/> 
+                        <input type ="email" className="form-control" onChange={(e) => setCorreo(e.target.value)}
+                        value={correo}/> 
                     </div>
                 </div>
                                 <div className="col-12">
                     <div className="mb-3">
                         <label>Contraseña</label>
-                        <input type ="text" className="form-control" onChange={(e) => setPassword(e.target.value)}/> 
+                        <input type ="text" className="form-control" onChange={(e) => setPassword(e.target.value)}
+                        value={password}/> 
                     </div>
                 </div>
                 <div className="col-12 d-flex gap-2 justify-content-end">
